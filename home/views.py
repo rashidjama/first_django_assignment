5eee3ba157eca393664c6a7c555ba3612760598a
Rashid Jama <rashidjama5@gmail.com>
2020-06-18 11:47:34 -0700

    first django assignment

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+  return HttpResponse( "placeholder to later display a list of all blogs")
+
+def new(request):
+  return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+  return redirect('/')
+
+def show(request, num):
+    return HttpResponse(f"Placeholder to display blog number {num}.")
+
+def edit(request, num):
+    return HttpResponse(f"Placeholder to edit blog {num}.")
+
+def destroy(request, num):
+    return redirect('/')
